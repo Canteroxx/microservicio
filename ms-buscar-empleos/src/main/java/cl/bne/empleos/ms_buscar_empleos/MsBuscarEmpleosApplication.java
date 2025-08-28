03c1c4569c8e55fc533a4cab6dcda9b77d6bfd46
Alvarez <vicente.alvarez2023@alu.uct.cl>
2025-08-28 19:39:25 -0400

    Creación del proyecto con SpringBoot y Maven. Se incluyen dependencias necesarias respetando la mini ficha y además se incluye una base de datos temporal para pruebas la cual es persistente

---
+++ ms-buscar-empleos/src/main/java/cl/bne/empleos/ms_buscar_empleos/MsBuscarEmpleosApplication.java
@@ -0,0 +1,13 @@
+package cl.bne.empleos.ms_buscar_empleos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBuscarEmpleosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsBuscarEmpleosApplication.class, args);
+	}
+
+}
---
+++ ms-buscar-empleos/src/test/java/cl/bne/empleos/ms_buscar_empleos/MsBuscarEmpleosApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.bne.empleos.ms_buscar_empleos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBuscarEmpleosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
